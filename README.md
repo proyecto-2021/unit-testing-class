7bde2ed126ecbd0a2125eb744ae087465502691f
Pablo Ponzio <pponzio@dc.exa.unrc.edu.ar>
2021-09-27 21:21:10 -0300

    Added missing files and README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# Requisitos 
+
+Instalar todos los paquetes listados en el archivo requirements.txt.
+
+# Ejecución de los tests para `maximo`
+
+En una terminal ejecute el comando:
+
+`python3 -m pytest test/test_maximo.py`
+
+# Ejecución de los tests para `add_user`
+
+En una terminal ejecute el comando:
+
+`python3 -m pytest test/test_user.py`
+
---
+++ daos/user_dao.py
@@ -0,0 +1,16 @@
+from app import User, db
+
+
+def add_user(name, mail):
+    if get_user_by_name(name) is not None:
+        raise ValueError(f'User with name {name} already in the DB')
+
+    user = User(username=name, email=mail)
+    db.session.add(user)
+    db.session.commit()
+
+
+def get_user_by_name(name):
+    return User.query.filter_by(username=name).first()
+
+
